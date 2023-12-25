29d2280008d887ecb30a9aac0ed5c35d52ba1a73
GolamAhammed <devgolamgit@gmail.com>
2023-12-25 20:35:49 +0530

    update landing pages

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,16 +5,18 @@ import Link from "next/link"
 const LandingPage = () => {
   return (
     <div className="ml-12">
-      Landing Page (unprotected)
+      <div className="mt-5 text-center text-sm md:text-xl "> 
+      Landing Page (unprotected) Under Devlopment 
+      </div>
       <div className="flex flex-direction-column s-between-12">
-        <Link href="/sign-in">
+        <Link href="/sign-in"  className="pl-12 mt-12 flex item-center">
         <Button>
           Login
         </Button>
         </Link>
 
 
-        <Link href="/sign-up">
+        <Link href="/sign-up" className="pl-12 mt-12 flex item-center">
         <Button>
           Register
         </Button>
