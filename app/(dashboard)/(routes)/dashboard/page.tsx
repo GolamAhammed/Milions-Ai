26631d6064f9d36e39e6a0ecdb341c18ae60e1e6
GolamAhammed <devgolamgit@gmail.com>
2023-12-25 15:53:11 +0530

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+        <div className="mb-8 space-y-0">
+          <h1 className="text-2xl md:text-4xl font-bold text-center ">
+            Explore the Power of AI
+          </h1>
+              <p className="text-muted-foreground font-light text-sm md:text-xl text-center">
+                Chat with the smartest Ai-Experience the Power of AI
+              </p>
+        </div>
+    </div>
+  )
+}
+
+export default DashboardPage
