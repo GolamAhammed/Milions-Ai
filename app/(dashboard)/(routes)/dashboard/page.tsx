e2cfc72fbe06195f08b1d32c210571ff9b3077f6
GolamAhammed <devgolamgit@gmail.com>
2023-12-26 17:22:08 +0530

    Your commit message here

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,19 @@
 
 
+import { Card } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
+import { MessageSquare } from 'lucide-react'
 import React from 'react'
+
+const tools = [
+  {
+    label: "Coversation",
+    icon: MessageSquare,
+    color:"text-violet-500",
+    bgColor: "bg-pink-500/10",
+    href:"/conversation"
+  }
+]
 
 const DashboardPage = () => {
   return (
@@ -12,6 +25,22 @@ const DashboardPage = () => {
               <p className="text-muted-foreground font-light text-sm md:text-xl text-center">
                 Chat with the smartest Ai-Experience the Power of AI
               </p>
+        </div>
+        <div className="px-4 md:px-20 lg:px-32 space-y-4">
+              {tools.map((tool)=> (
+             <Card 
+             key={tool.href}
+             className='p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer '
+             >
+              <div className="">
+                <div className={cn("",tool.bgColor)} >
+                  <tool.icon className ={cn ("flex items-center flex-1", tool.color)} />
+
+                </div>
+
+              </div>
+              </Card>
+              ))}
         </div>
     </div>
   )
